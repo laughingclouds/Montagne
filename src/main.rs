a9fe21f5e8b8dab18b7748846166267a99005b23
Hemant Bhandari <zenithtaiga@gmail.com>
2025-05-13 02:29:18 +0530

    Start with text editor widget

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+#![windows_subsystem = "windows"]
+
+use::iced::widget::text_editor;
+use::iced::Element;
+
+
+// define state
+#[derive(Default)]
+struct MdEditor {
+    content: text_editor::Content,
+}
+
+
+// define messages (interactions of the application)
+#[derive(Debug, Clone)]
+enum Message {
+    Edit(text_editor::Action),
+}
+
+
+impl MdEditor {
+    fn view(&self) -> Element<'_, Message> {
+        text_editor(&self.content)
+            .on_action(Message::Edit)
+            .into()
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Edit(action) => {
+                self.content.perform(action);
+            }
+        }
+    }
+}
+
+fn main() -> iced::Result {
+    iced::run("mdEdit", MdEditor::update, MdEditor::view)
+}
