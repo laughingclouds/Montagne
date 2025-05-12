8dac7a4b9d6d860d25b3fb8f64d6f57c9563c80f
Hemant Bhandari <zenithtaiga@gmail.com>
2025-05-13 02:32:08 +0530

    rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 #![windows_subsystem = "windows"]
 
-use::iced::widget::text_editor;
-use::iced::Element;
-
+use ::iced::Element;
+use ::iced::widget::text_editor;
 
 // define state
 #[derive(Default)]
@@ -10,19 +9,15 @@ struct MdEditor {
     content: text_editor::Content,
 }
 
-
 // define messages (interactions of the application)
 #[derive(Debug, Clone)]
 enum Message {
     Edit(text_editor::Action),
 }
 
-
 impl MdEditor {
     fn view(&self) -> Element<'_, Message> {
-        text_editor(&self.content)
-            .on_action(Message::Edit)
-            .into()
+        text_editor(&self.content).on_action(Message::Edit).into()
     }
 
     fn update(&mut self, message: Message) {
