67fb9ecf29d66c92ea8e09e1d8470041b28e6924
Hemant Bhandari <zenithtaiga@gmail.com>
2025-05-13 17:37:52 +0530

    Load an .md file as plaintext | Change window title

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,37 @@
 #![windows_subsystem = "windows"]
 
+use std::fs::File;
+use std::io::Read;
+use std::path::Path;
+
 use ::iced::Element;
 use ::iced::widget::text_editor;
 
 // define state
-#[derive(Default)]
 struct MdEditor {
     content: text_editor::Content,
+}
+
+impl Default for MdEditor {
+    fn default() -> Self {
+        let path = Path::new("target/README.md");
+        let display = path.display();
+
+        let mut file = match File::open(&path) {
+            Err(why) => panic!("Couldn't open {}: {}", display, why),
+            Ok(file) => file,
+        };
+
+        let mut s = String::new();
+
+        if let Err(why) = file.read_to_string(&mut s) {
+            panic!("Couldn't read {}: {}", display, why);
+        }
+
+        Self {
+            content: text_editor::Content::with_text(&s),
+        }
+    }
 }
 
 // define messages (interactions of the application)
@@ -30,5 +55,5 @@ impl MdEditor {
 }
 
 fn main() -> iced::Result {
-    iced::run("mdEdit", MdEditor::update, MdEditor::view)
+    iced::run("Montagne", MdEditor::update, MdEditor::view)
 }
