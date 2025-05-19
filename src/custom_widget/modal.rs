0a004f61d7fe6a82373d53c7762137cae5f10e9a
Hemant Bhandari <zenithtaiga@gmail.com>
2025-05-19 13:21:47 +0530

    custom_widget directory

---
+++ src/custom_widget/modal.rs
@@ -0,0 +1,43 @@
+use crate::montagne_theme::modal_style;
+
+use iced::{
+    Border, Color, Element,
+    border::Radius,
+    widget::{button, center, column, container, opaque, row, stack, text},
+};
+
+use crate::message::Message;
+
+pub fn exit_modal<'a>(base: impl Into<Element<'a, Message>>) -> Element<'a, Message> {
+    stack![
+        base.into(),
+        opaque(
+            center(opaque(
+                container(
+                    column![
+                        text("You have unsaved work. Save changes?"),
+                        row![
+                            button("Save").on_press(Message::SaveFile),
+                            button("Close without saving").on_press(Message::CloseApp),
+                            button("Go back").on_press(Message::CloseExitModal),
+                        ]
+                        .spacing(10)
+                    ]
+                    .spacing(10)
+                    .padding(30)
+                )
+                .style(|_them| container::Style {
+                    text_color: Some(Color::WHITE),
+                    border: Border {
+                        color: Color::WHITE,
+                        width: 1.0,
+                        radius: Radius::new(8.0)
+                    },
+                    ..container::Style::default()
+                })
+            ))
+            .style(modal_style)
+        ),
+    ]
+    .into()
+}
